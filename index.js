f42e6f7a53ce5b5d5fa2e9b9e8e7f2559413536b
khadi1 <64810011+khadi1@users.noreply.github.com>
2021-08-07 12:02:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const app = express()
 
 
 
-// const middleware = (req, res, next) => {
-//     const daterequest = new Date()
-//     let day = daterequest.getDay();
-//     let hour = daterequest.getHours();
-//     if (day > 0 && day < 5 && hour <= 17 && hour >= 9)
-//         return next();
-//     else res.send('<h1>we are closed now</h1>');
+const middleware = (req, res, next) => {
+    const daterequest = new Date()
+    let day = daterequest.getDay();
+    let hour = daterequest.getHours();
+    if (day > 0 && day < 5 && hour <= 17 && hour >= 9)
+        return next();
+    else res.send('<h1>we are closed now</h1>');
 
-// }
+}
 
 
-// app.use(middleware);
+app.use(middleware);
 const port = 5500;
 
 
